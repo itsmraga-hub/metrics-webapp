8c1d89c6190a34ca303ac2d6623a5a6586502d54
Raga <ragawilliam570@gmail.com>
2022-10-03 18:08:57 +0300

    Add Provider to App and router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchHomePageCases } from "./redux/covidcases";
+import Home from "./components/Home";
+
 const App = () =>{
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchHomePageCases())
+  }, []);
+
   return (
-    <div className="App">
-    </div>
+    <>
+    <Home />
+    </>
   );
 }
 

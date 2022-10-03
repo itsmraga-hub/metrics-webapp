d49ff5f049b62103ed31003e61d2bf3af224643a
Raga <ragawilliam570@gmail.com>
2022-10-03 21:14:40 +0300

    Add Menu component and Home page view

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import { useSelector } from "react-redux";
 
 import CasesContainer from "./CasesContainer"
+import Header from "./Header";
+import Menu from "./Menu";
 
 const Home = () => {
   let cases = useSelector((state) => state.cases);
 
   return (
     <>
+      <Header />
+      <Menu />
       <CasesContainer cases={cases}/>
     </>
   )
